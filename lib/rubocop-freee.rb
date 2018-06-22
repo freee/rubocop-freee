6de9db5344fcf7e82a69d719fad29345b7ecefd8
Ryota Ikezawa <ikezawa-ryota@c-fo.com>
2018-06-22 13:06:57 +0900

    Copy rules from private repository

---
+++ lib/rubocop-freee.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'pathname'
+require 'yaml'
+
+require 'rubocop'
+require 'rubocop-freee'
+require 'rubocop/freee/version'
+
+# custom cops
+require 'rubocop/cop/scope_return_value'
---
+++ spec/rubocop/cop/freee/scope_return_value_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Freee::ScopeReturnValue do
+  subject(:cop) { described_class.new }
+
+  let(:good_source) do
+    <<-SOURCE
+      class Test < ActiveRecord::Base
+        scope :mine1, ->(id) { where(id: id) }
+        scope :mine2, -> do
+          where(id: self.company.id)
+        end
+        def self.a(arg1, arg2)
+          arg1 * arg2 + self.id
+        end
+        def b(arg1, arg2)
+          arg1 * arg2
+        end
+      end
+    SOURCE
+  end
+
+  let(:bad_source) do
+    <<-SOURCE
+      class Test < ActiveRecord::Base
+        scope :mine1, -> { nil;self }
+        scope :mine2, ->(id) do
+          if id.nil?
+            self
+          else
+            where(id: id)
+          end
+        end
+        def self.a(arg1, arg2)
+          arg1 * arg2 + self.id
+        end
+        def b(arg1, arg2)
+          arg1 * arg2
+        end
+      end
+    SOURCE
+  end
+
+  context 'good' do
+    it do
+      inspect_source(cop, good_source)
+      expect(cop.offenses).to be_empty
+    end
+  end
+
+  context 'bad' do
+    it do
+      inspect_source(cop, bad_source)
+      expect(cop.offenses.size).to eq(2)
+      expect(cop.message).to eq('scopeでselfを返してはいけません...')
+    end
+  end
+end
